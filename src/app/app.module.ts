aaa5afc27fa022e30a04aa573fecb63d940fc00b
Luigidls <10180259@ucn.dk>
2022-10-25 19:20:58 +0200

    Landing-page_Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './features/footer/footer.component';
 import { NavbarComponent } from './features/navbar/navbar.component';
-import { FrontPageModule } from './pages/front-page/front-page.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PortfolioPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FrontPageModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/footer/footer.component.ts
+++ src/app/features/footer/footer.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'foot',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+
+  faGithub = faGithub;
+  faEnvelope = faEnvelope;
+  faLinkedin = faLinkedin;
 
   constructor() { }
 
